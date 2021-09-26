a7615a1950f5746f6f3ef33d7819f9b9b67f9b61
vikas Punde <vikaspunde78@gmail.com>
2021-09-26 21:16:38 +0530

    word counter service

--- src/main/java/com/lib/word/controller/WordCounterController.java
+++ src/main/java/com/lib/word/controller/WordCounterController.java
@@ -4,17 +4,19 @@ import com.lib.word.service.WordCounterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class WordCounterController {
 
     @Autowired
     private WordCounterService wordCounterService;
 
     @GetMapping("/wordcounter/{word}")
-    public Integer getWordCounter(@PathVariable String word){
-            return wordCounterService.getWordCount(word);
+    public Integer getWordCounter(@PathVariable String word) {
+        return wordCounterService.getWordCount(word);
     }
 
 }
